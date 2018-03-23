8d24e9b45b1fce53275e3ffb918452b45c2bdee0
melchor629 <melchor9000@gmail.com>
2018-03-23 22:22:02 +0100

    Get port from environment

--- app.py
+++ app.py
@@ -120,4 +120,7 @@ Audio URL: {1}</code></pre>
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if os.environ['PORT']:
+        app.run(host='127.0.0.1', port=int(os.environ['PORT'])
+    else:
+        app.run(host='127.0.0.1', port=5000)
